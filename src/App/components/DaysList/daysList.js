a1144705222793ef4469102e19c28be9af46d231
HanaPos <h.hedbavna@gmail.com>
2020-11-29 13:55:11 +0100

    meal detail

--- src/App/components/DaysList/daysList.js
+++ src/App/components/DaysList/daysList.js
@@ -2,26 +2,16 @@ import React from "react";
 import { Day } from "../Day/day";
 
 export const DaysList = ({ dataJidelnicek, onMealDetail }) => {
+  const onDetail = (meal) => {
+    console.log("daylist: ", meal);
+    onMealDetail(meal);
+  };
 
-     const onDetail = meal => {console.log("daylist: ", meal);
-     onMealDetail(meal);
-}
-     
   console.log(dataJidelnicek);
   const dataJidelnicekPole = [...dataJidelnicek.values()];
 
-  const rectCmpDen = dataJidelnicekPole.map((den,index) => {
-    return (
-      <Day
-        key={index}
-        day={den}
-        onMealDetail={onDetail}/>
-    );
+  const rectCmpDen = dataJidelnicekPole.map((den, index) => {
+    return <Day key={index} day={den} onMealDetail={onDetail} />;
   });
-  return (
-    <div>
-      <h1>DayList</h1>
-      {rectCmpDen}
-    </div>
-  );
+  return <div>{rectCmpDen}</div>;
 };
--- src/App/components/Detail/detail.js
+++ src/App/components/Detail/detail.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './detail.css';
+import React from "react";
+import "./detail.css";
 
-
-export const Detail = ({mealInfo}) => (
-<div className="detail">
-
-{mealInfo.cas}
-   
-
-     
-</div>
- 
+export const Detail = ({ mealInfo }) => (
+  <div className="detail">
+    <h2 className="meal-name">{mealInfo.nazev}</h2>
+    <div className="ingredience-list">
+      {mealInfo.ingredience.map((ing) => (
+        <div className="ingredience">{`${ing.nazev} (${ing.mnozstvi} ${ing.jednotka})`}</div>
+      ))}
+    </div>
+  </div>
 );
