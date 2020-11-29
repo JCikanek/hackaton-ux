c7eb9e7d07b0d010ad0a51278a037f4b9671090f
HanaPos <h.hedbavna@gmail.com>
2020-11-29 10:32:09 +0100

    header date added

--- src/App/components/Header/header.js
+++ src/App/components/Header/header.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./header.css";
 
-export const Header = () => (
-  <div className="App-header">
-    <h1>Týdenní menu</h1>
-  </div>
-);
+export const Header = () => {
+  const now = new Date();
+  const dateFormated = new Intl.DateTimeFormat("cs-CZ", {
+    weekday: "short",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(now);
+
+  return (
+    <div className="App-header">
+      <h1>Týdenní menu</h1>
+      <div className="date">{dateFormated}</div>
+    </div>
+  );
+};
