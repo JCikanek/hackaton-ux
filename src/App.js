0ba5e48471fd5960003f730eef47bdbc0cc60c4a
Vera Jancurova <jancurova.vera@gmail.com>
2020-11-29 09:49:23 +0100

    components 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './App/components/Header/header';
+import { DaysList } from './App/components/DaysList/daysList';
 
 function App() {
   return (
     <div className="App"> 
-    {"Hello Hacatlon"}
+     <Header />
+     <DaysList />
     </div>
   );
 }
---
+++ src/App/components/DaysList/daysList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const DaysList = () => (
+<div>
+
+     <h>DayList</h>  
+
+     
+</div>
+ 
+);
