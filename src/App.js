9b2e014c1126c68b76ef10b567111bd1c85c9427
HanaPos <h.hedbavna@gmail.com>
2020-11-29 15:03:53 +0100

    fetch crud

--- src/App.js
+++ src/App.js
@@ -5,31 +5,35 @@ import { DaysList } from "./App/components/DaysList/daysList";
 import { Detail } from "./App/components/Detail/detail";
 
 import { Day } from "./App/components/Day/day";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import fakeData from "./jidla.json";
 
-const url = "https://crudcrud.com/api/d6ffc80c74d94c669356ad099c86701d/meals";
+const url = "/meals";
 const readData = (url) => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  return fetch(url).then((res) => res.json());
+  /* .then((data) => {
+      console.log(data); 
+    });*/
 };
-readData(url);
+const groupData = (data) =>
+  data.reduce((acc, meal) => {
+    if (acc?.has(meal.datum)) {
+      acc.get(meal.datum).push(meal);
+    } else {
+      acc.set(meal.datum, [meal]);
+    }
+    return acc;
+  }, new Map());
 
-const fakeDataMap = fakeData.reduce((acc, meal) => {
-  if (acc?.has(meal.datum)) {
-    acc.get(meal.datum).push(meal);
-  } else {
-    acc.set(meal.datum, [meal]);
-  }
-  return acc;
-}, new Map());
-console.log(fakeDataMap);
+//readData(url);
 
 function App() {
-  const [state, setState] = useState(fakeDataMap);
+  const [state, setState] = useState([]);
   const [selectedMeal, setSelectedMeal] = useState();
 
+  useEffect(() => {
+    readData(url).then((data) => setState(groupData(data)));
+  });
   const displayMeal = (meal) => {
     setSelectedMeal(meal);
     console.log("app:", meal);
