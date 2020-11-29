5e9cd1a0a3ec59b4117aa2764515bfe8457e26cb
HanaPos <h.hedbavna@gmail.com>
2020-11-29 13:04:55 +0100

    sorting meals by time

--- src/App/components/DaysList/daysList.js
+++ src/App/components/DaysList/daysList.js
@@ -4,9 +4,18 @@ import { Day } from "../Day/day";
 export const DaysList = ({ dataJidelnicek }) => {
   console.log(dataJidelnicek);
   const dataJidelnicekPole = [...dataJidelnicek.values()];
-  const rectCmpDen = dataJidelnicekPole.map((den) => (
-    <Day key={den} day={den} />
-  ));
+  const rectCmpDen = dataJidelnicekPole.map((den) => {
+    return (
+      <Day
+        key={den}
+        day={den.sort(
+          (a, b) =>
+            parseInt(a.cas.match(/\d/g).join("")) -
+            parseInt(b.cas.match(/\d/g).join(""))
+        )}
+      />
+    );
+  });
 
   return (
     <div>
